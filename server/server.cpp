8ad405c0bf75a7fe6a48bbe79176a44bd1510f2d
Michael Telakowicz <mat210@uakron.edu>
2024-03-06 00:22:00 -0500

    Update server with websocket library

--- server/server.cpp
+++ server/server.cpp
@@ -1,4 +1,9 @@
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
 #include <iostream>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
 
 int main()
 {
