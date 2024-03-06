4a822e8cc5f74cde6c25db248745a1469469070d
Michael Telakowicz <mat210@uakron.edu>
2024-03-06 00:21:53 -0500

    Update client with websocket library

--- client/client.cpp
+++ client/client.cpp
@@ -1,4 +1,9 @@
+#include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+
 #include <iostream>
+
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 int main()
 {
