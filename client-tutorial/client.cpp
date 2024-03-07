9f60ff005b03874ca89f01af3fdfa50a3e7a0911
TSNanites <aydenmr1108@gmail.com>
2024-03-06 19:51:09 -0500

    Server Tutorial Skeleton

--- client-tutorial/client.cpp
+++ client-tutorial/client.cpp
@@ -6,6 +6,22 @@
 using namespace std;
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
+ 
+class websocket_endpoint {
+public:
+    websocket_endpoint () {
+        m_endpoint.clear_access_channels(websocketpp::log::alevel::all);
+        m_endpoint.clear_error_channels(websocketpp::log::elevel::all);
+ 
+        m_endpoint.init_asio();
+        m_endpoint.start_perpetual();
+ 
+        m_thread.reset(new websocketpp::lib::thread(&client::run, &m_endpoint));
+    }
+private:
+    client m_endpoint;
+    websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
+};
 
 int main() {
     bool done = false;
