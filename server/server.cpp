a076bd34345183603efcb4cd486bfe36005a57b4
Michael Telakowicz <mat210@uakron.edu>
2024-03-12 19:50:11 -0400

    Add listConnections to ServerHandler

--- server/server.cpp
+++ server/server.cpp
@@ -23,18 +23,38 @@ public:
         echo_server.set_access_channels(websocketpp::log::alevel::all);
         echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
+        // Define handler to track connections
+        echo_server.set_open_handler(websocketpp::lib::bind(&ServerHandler::onOpen, this, &echo_server, ::_1));
+
+        // Define handler to remove closed connections from the vector
+        echo_server.set_close_handler(websocketpp::lib::bind(&ServerHandler::onClose, this, &echo_server, ::_1));
+
         // Initialize Asio
         echo_server.init_asio();
 
         // Register our message handler
-        echo_server.set_message_handler(websocketpp::lib::bind(&ServerHandler::on_message, this, &echo_server, ::_1, ::_2));
+        echo_server.set_message_handler(websocketpp::lib::bind(&ServerHandler::onMessage, this, &echo_server, ::_1, ::_2));
     }
 
     ~ServerHandler() {
         echo_server.stop_listening();
     }
 
-    void on_message(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
+    void onOpen(server *s, websocketpp::connection_hdl hdl) {
+        connections.push_back(hdl);
+    }
+
+    void onClose(server *s, websocketpp::connection_hdl hdl) {
+        auto it = std::find_if(connections.begin(), connections.end(), [&](const websocketpp::connection_hdl& item) {
+            return hdl.lock() == item.lock();
+        });
+        if (it != connections.end()) {
+            connections.erase(it);
+        }
+    }
+
+    void onMessage(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
+        listConnections();
         // std::cout << "on_message called with hdl: " << hdl.lock().get()
         //           << " and message: " << msg->get_payload()
         //           << std::endl;
@@ -65,7 +85,20 @@ public:
 
         // Start the ASIO io_service run loop
         echo_server.run();
-    }    
+    }
+
+    void listConnections() {
+        for (int i = 0; i < connections.size(); i++) {
+            auto connection = echo_server.get_con_from_hdl(connections[i]);
+
+            // Get the ip and port of the connection from the socket with boost::asio
+            auto& socket = connection->get_raw_socket();
+            std::string ip = socket.lowest_layer().remote_endpoint().address().to_string();
+            std::string port = std::to_string(socket.lowest_layer().remote_endpoint().port());
+
+            std::cout << i << ": " << ip << " Port: " << port << std::endl;
+        }
+    }
 };
 
 // Commands:
