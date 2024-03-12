b7d8fa3dc05c20c0f615f737fb0ca3a0d3bf5bc1
nobrien1234 <omgnateobrien@gmail.com>
2024-03-12 17:50:46 -0400

    Add functionality for server

--- server/server.cpp
+++ server/server.cpp
@@ -5,6 +5,15 @@
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
+// Commands:
+// help
+// list-connections
+//      ex: 0: ws://10.10.100.10:9002
+// message-history
+// send-string <message>
+// disconnect <index>
+// quit
+
 int main()
 {
     std::cout << "Server Succesfully Ran";
