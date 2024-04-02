996e4a6b128621d44c4c28fe07dc5d1ac3e51912
Michael Telakowicz <mat210@uakron.edu>
2024-04-02 18:10:58 -0400

    Added help and message history command

--- server/server.cpp
+++ server/server.cpp
@@ -23,36 +23,34 @@ typedef server::message_ptr message_ptr;
 
 // // Commands:
 // // help
-// // list-connections
+// // list-connections  done
 // //      ex: 0: ws://10.10.100.10:9002
-// // message-history
-// // send-string <message>
-// // disconnect <index>
-// // quit
-
+// // message-history done
+// // send-string <message> done
+// // disconnect <index>   done
+// // quit done
 
 class ServerHandler
 {
 public:
     ServerHandler() : next_id(0)
     {
+        // Set logging settings
         serverEndpoint.set_access_channels(websocketpp::log::alevel::all);
         serverEndpoint.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
+        // Initialize the server & start the perpetual IO service
         serverEndpoint.init_asio();
         serverEndpoint.start_perpetual();
 
+        // Set the handlers
         serverEndpoint.set_open_handler(websocketpp::lib::bind(&ServerHandler::onOpen, this, &serverEndpoint, ::_1));
-
         serverEndpoint.set_message_handler(websocketpp::lib::bind(&ServerHandler::onMessage, this, &serverEndpoint, ::_1, ::_2));
-
         serverEndpoint.set_close_handler(websocketpp::lib::bind(&ServerHandler::onClose, this, &serverEndpoint, ::_1));
 
+        // Start listening for new connections on port 9002
         serverEndpoint.listen(9002);
-
-        // Start the server accept loop
         serverEndpoint.start_accept();
-
         thread.reset(new websocketpp::lib::thread(&server::run, &serverEndpoint));
     }
 
@@ -72,15 +70,21 @@ public:
         }
     }
 
+    // When a new connection is opened
     void onOpen(server *s, websocketpp::connection_hdl hdl)
     {
+        // add the new connection to the list of connections
         connections.push_back(hdl);
-        std::cout << "test" << std::endl;
     }
 
+    // When a message is recieved
     void onMessage(server *s, websocketpp::connection_hdl hdl, message_ptr msg)
     {
+
         std::cout << msg->get_payload() << std::endl;
+
+        // Add the recieved message to the message history
+        messageHistory.push_back("recieved: " + msg->get_payload());
 
         // std::cout << "on_message called with hdl: " << hdl.lock().get()
         //           << " and message: " << msg->get_payload()
@@ -103,24 +107,44 @@ public:
         // }
     }
 
+    // When a connection is closed
     void onClose(server *s, websocketpp::connection_hdl hdl)
     {
+        // remove the connection from the list of connections
         auto it = std::find_if(connections.begin(), connections.end(), [&](const websocketpp::connection_hdl &item)
                                { return hdl.lock() == item.lock(); });
-
         if (it != connections.end())
         {
             connections.erase(it);
         }
     }
+
+    /**
+     * Display the help text
+    */
+    void help() {
+        std::cout
+            << "\nCommand List:\n"
+            << "help: Display this help text\n"
+            << "send <message>: Send a message to all connected clients\n"
+            << "list-connections: List all the connections\n"
+            << "message-history: List the message history\n"
+            << "disconnect <index>: Disconnect a client\n"
+            << "quit: Exit the program\n"
+            << std::endl;
+    }
     
+    /**
+     * Send a message to all connected clients
+     * 
+     * @param[in] msg The message to send
+    */
     void send(std::string msg)
     {
         websocketpp::lib::error_code ec;
 
         for (auto hdl : connections)
         {
-
             serverEndpoint.send(hdl, msg, websocketpp::frame::opcode::value::text, ec);
 
             if (ec)
@@ -129,8 +153,14 @@ public:
                 continue;
             }
         }
+
+        // Add the sent message to the message history
+        messageHistory.push_back("sent: " + msg);
     }
 
+    /**
+     * List all the connections
+    */
     void listConnections() {
         for (int i = 0; i < connections.size(); i++) {
             auto connection = serverEndpoint.get_con_from_hdl(connections[i]);
@@ -144,6 +174,20 @@ public:
         }
     }
 
+    /**
+     * List the message history
+    */
+    void listMessageHistory() {
+        for (int i = 0; i < messageHistory.size(); i++) {
+            std::cout << messageHistory[i] << std::endl;
+        }
+    }
+
+    /**
+     * Disconnect a client
+     * 
+     * @param[in] index The index of the connection to disconnect
+    */
     void disconnect(int index) {
         if (index < 0 || index >= connections.size()) {
             std::cout << "Invalid index" << std::endl;
@@ -154,6 +198,11 @@ public:
         serverEndpoint.close(connection->get_handle(), websocketpp::close::status::normal, "Disconnecting");
     }
 
+    /**
+     * Get the local ip address
+     * 
+     * @note This is a linux specific command
+    */
     void getLocalIp()
     {
         std::cout << "IP: \n";
@@ -164,8 +213,14 @@ private:
     server serverEndpoint;
     websocketpp::lib::shared_ptr<websocketpp::lib::thread> thread;
 
-    std::vector<websocketpp::connection_hdl> connections;
-    int next_id;
+    std::vector<websocketpp::connection_hdl> connections; // List of connections
+    /**
+     * Message history
+     * smaller the index, the older the message
+     * format: ["recieved: <message>", "sent: <message>"]
+     * */ 
+    std::vector<std::string> messageHistory;
+    int next_id; 
 };
 
 int main()
@@ -187,12 +242,7 @@ int main()
         }
         else if (input == "help")
         {
-            std::cout
-                << "\nCommand List:\n"
-                << "send <message>"
-                << "help: Display this help text\n"
-                << "quit: Exit the program\n"
-                << std::endl;
+            endpoint.help();
         }
         else if (input.substr(0, 4) == "send")
         {
@@ -202,6 +252,10 @@ int main()
         else if (input == "list-connections")
         {
             endpoint.listConnections();
+        }
+        else if (input == "message-history")
+        {
+            endpoint.listMessageHistory();
         }
         else if (input.substr(0, 10) == "disconnect")
         {
